1dca9185afb5a0dd5ab5102b4611ca8294b4db62
akitra13 <errongaming26@gmail.com>
2025-05-27 10:00:40 +0700

    update database

---
+++ Controllers/C_User.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using JALOKA.Database;
+using JALOKA.Models;
+using Npgsql;
+
+namespace JALOKA.Controllers
+{
+    public class C_User
+    {
+        public bool Login(string id_pelajar, string password)
+        {
+            using (var db = new Connector())
+            {
+                var conn = db.GetConnection();
+                var cmd = new NpgsqlCommand("SELECT COUNT(*) FROM users WHERE id_pelajar = @id_pelajar AND password = @password", conn);
+                cmd.Parameters.AddWithValue("@id_pelajar", id_pelajar);
+                cmd.Parameters.AddWithValue("@password", password);
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+                return count > 0;
+            }
+        }
+
+        public bool Register(M_User user)
+        {
+            using (var db = new Connector())
+            {;
+                var conn = db.GetConnection();
+                var cmd = new NpgsqlCommand(@"INSERT INTO users (id_pelajar, password, nama, email, nomor_hp, alamat) VALUES (@id_pelajar, @password, @nama, @email, @nomor_hp, @alamat)", conn);
+                cmd.Parameters.AddWithValue("@id_pelajar", user.id_pelajar);
+                cmd.Parameters.AddWithValue("@password", user.password);
+                cmd.Parameters.AddWithValue("@nama", user.nama);
+                cmd.Parameters.AddWithValue("@email", user.email);
+                cmd.Parameters.AddWithValue("@nomor_hp", user.nomor_hp);
+                cmd.Parameters.AddWithValue("@alamat", user.alamat);
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        }
+
+        public List<M_User> GetAll()
+        {
+            var list = new List<M_User>();
+            using (var db = new Connector())
+            {
+                var conn = db.GetConnection();
+                conn.Open();
+                var cmd = new NpgsqlCommand("SELECT * FROM users", conn);
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    list.Add(new M_User
+                    {
+                        id_pelajar = reader["id_pelajar"].ToString(),
+                        password = reader["password"].ToString(),
+                        nama = reader["nama"].ToString(),
+                        email = reader["email"].ToString(),
+                        nomor_hp = reader["nomor_hp"].ToString(),
+                        alamat = reader["alamat"].ToString()
+                    });
+                }
+            }
+            return list;
+        }
+    }
+}
---
+++ Models/M_User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JALOKA.Models
+{
+    public class M_User
+    {
+        public string id_pelajar { get; set; }
+        public string password { get; set; }
+        public string nama { get; set; }
+        public string email { get; set; }
+        public string nomor_hp { get; set; }
+        public string alamat { get; set; }
+    }
+}
