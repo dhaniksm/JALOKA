afaacce5c619e16c6d180ba8b15bafe724371db4
1LH4MDC <ilhamdwicahya338@gmail.com>
2025-05-29 17:54:13 +0700

    view semua fitur admin

--- Views/V_Datapengguna.cs
+++ Views/V_Datapengguna.cs
@@ -10,11 +10,41 @@ using System.Windows.Forms;
 
 namespace JALOKA.Views
 {
-    public partial class V_Datapengguna: Form
+    public partial class V_Datapengguna : Form
     {
         public V_Datapengguna()
         {
             InitializeComponent();
+        }
+
+        private void buttonDashboard_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonManajemenBuku_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDataPengguna_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPengembalian_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonRiwayatPeminjaman_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonKeluar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
