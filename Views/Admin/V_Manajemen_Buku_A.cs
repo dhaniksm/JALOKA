1d1a4c41b33c8455ab659fb79fb26d7fd38ef359
akitra13 <errongaming26@gmail.com>
2025-06-14 03:42:56 +0700

    error

---
+++ Views/Admin/V_Manajemen_Buku_A.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JALOKA.Views.Admin
+{
+    public partial class V_Manajemen_Buku_A: Form
+    {
+        public V_Manajemen_Buku_A()
+        {
+            InitializeComponent();
+        }
+    }
+}
